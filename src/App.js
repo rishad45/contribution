e1371e452ae59025fd659c90b8a6ab700738ac8b
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2022-11-30 20:05:29 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
+import NavBar from "./Components/NavBar/NavBar";
 import Mian from "./Pages/Main/Mian";
 
 
 function App() {
   return (
     <Fragment>
+      <NavBar />
        <Mian></Mian>
     </Fragment>
   );
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div>
+        <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+    </div>
+  )
+}
+
+export default NavBar
