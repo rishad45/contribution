bb91317b2772c22116123e6611ff9cffbc1413f7
MUHAMMEDANSIF <muhammadansif9633@gmail.com>
2022-11-30 11:15:48 +0530

    main

---
+++ src/Components/Proflle/Profile.jsx
@@ -0,0 +1,75 @@
+import React, { Fragment } from 'react'
+import './Profile.css'
+
+function Profile() {
+    return (
+        <Fragment>
+            <div className='main'>
+            <div className='sidebar'>
+              sidebar
+            </div>
+            <div className='center'>
+            <div className='container'>
+                <div className='row'>
+                    <div class="card col-md-4 mt-5">
+                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" class="card-img-top" alt="Fissure in Sandstone" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#!" class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+
+                    <div class="card col-md-4 mt-5">
+                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" class="card-img-top" alt="Fissure in Sandstone" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#!" class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+
+                    <div class="card col-md-4 mt-5">
+                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" class="card-img-top" alt="Fissure in Sandstone" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#!" class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+
+                    <div class="card col-md-4 mt-5">
+                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" class="card-img-top" alt="Fissure in Sandstone" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#!" class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+
+                    <div class="card col-md-4 mt-5">
+                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" class="card-img-top" alt="Fissure in Sandstone" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#!" class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+
+                    <div class="card col-md-4 mt-5">
+                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" class="card-img-top" alt="Fissure in Sandstone" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#!" class="btn btn-primary">Button</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Profile
---
+++ src/Pages/Main/Mian.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import Profile from '../../Components/Proflle/Profile'
+
+function Mian() {
+  return (
+    <Fragment>
+       <Profile></Profile>
+    </Fragment>
+  )
+}
+
+export default Mian
+
